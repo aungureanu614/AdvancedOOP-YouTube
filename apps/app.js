e17b718214b984fb6b2c819c1c6386bde6a4f731
aungureanu614 <aungureanu614@gmail.com>
2016-08-19 07:16:24 -0700

    comments

--- apps/app.js
+++ apps/app.js
@@ -3,23 +3,24 @@ $(function() {
         event.preventDefault();
 
         var request = gapi.client.youtube.search.list({
-            q: $('#query').val(),
+            q: $('#query').val(), //get user input
             part: 'snippet',
             type: "video",
             maxResults: 5
         });
         request.execute(function(response) {
             var results = response.result;
-            showResults(results);
+            showResults(results); 
         });
     });
 });
 
-
+//create video object
 var Video = function(videoId) {
     this.videoHTML = "<iframe src=//www.youtube.com/embed/" + videoId + "></iframe>";
 };
 
+//iterate through results and create a new instance of the video object with each new videoId that then gets appended to the HTML document
 function showResults(results) {
     $.each(results.items, function(index, item) {
         var vid = new Video(item.id.videoId);
@@ -32,6 +33,6 @@ function showResults(results) {
 function init() {
     gapi.client.setApiKey('AIzaSyAAeEliIWNKfAlOjtTFoEfGoTtqt_RAF0I');
     gapi.client.load("youtube", "v3", function() {
-        //yt api is ready
+        //youtube api is ready
     });
 }
